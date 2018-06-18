71e669ea3a0a3c3de54bca23bc807d2a021538c4
Iiro Puhakka <iiropuhakka@luukku.com>
2018-06-18 12:15:40 +0300

    Renamed project folder

--- RegressionAnalysisTest/src/ModelFit_tests.cs
+++ RegressionAnalysisTest/src/ModelFit_tests.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
 using NUnit.Framework;
-using RegressionAnalysis.Exception;
 using RegressionAnalysis;
+using RegressionAnalysis.Exception;
 
 namespace RegressionAnalysisTest
 {
